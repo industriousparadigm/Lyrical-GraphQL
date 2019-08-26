9be20077baf3af756b3a50e4dad221f5aadca4dd
mrsalvini <dsgmcosta@gmail.com>
2019-08-26 13:59:26 +0100

    renders a list of songs

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
-const SongList = () => {
-  return (
-    <ul>
-      SongList is here!
-    </ul>
-  )
+class SongList extends React.Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => <li className="collection-item" key={song.id}>{song.title}</li>)
+  }
+
+  render() {
+    if (this.props.data.loading) return <h1>Loading</h1>
+    return (
+      <ul className="collection">
+        {this.renderSongs()}
+      </ul >
+    )
+  }
 }
 
-export default SongList
+const query = gql`
+{
+  songs {
+    id
+    title
+  }
+}
+`
+
+export default graphql(query)(SongList)
